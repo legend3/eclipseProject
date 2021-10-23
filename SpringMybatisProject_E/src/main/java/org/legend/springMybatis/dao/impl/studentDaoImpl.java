534286fc426702311369547636e5c4f1a005b955
legend3 <zhenguo50@126.com>
2021-10-24 00:02:07 +0800

    dao被充当mapper类及用spring容器注入sqlSessionFactory在studentDaoImpl实现方法中完成SqlSession的创建

--- SpringMybatisProject_E/src/main/java/org/legend/springMybatis/dao/impl/studentDaoImpl.java
+++ SpringMybatisProject_E/src/main/java/org/legend/springMybatis/dao/impl/studentDaoImpl.java
@@ -5,11 +5,21 @@ import org.legend.springMybatis.entity.Student;
 import org.legend.springMybatis.mapper.StudentMapper;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
+/**
+ * studentDaoImpl实现接口中完成SqlSession的创建
+ * @author Administrator
+ *
+ */
 public class studentDaoImpl extends SqlSessionDaoSupport implements StudentMapper {
     @Override
     public void addStudent(Student student) {
+		/*
+		 * 2.studentDaoImpl继承SqlSessionDaoSupport;
+		 * 让studentDaoImpl具有sqlSessionFactory属性(在bean中完成属性值注入); 
+		 * 从而能创建SqlSession对象
+		 */
         SqlSession session = super.getSqlSession();
         StudentMapper stuDao = session.getMapper(StudentMapper.class);
-        stuDao.addStudent(student);
+        stuDao.addStudent(student);//接口映射会根据映射文件中sql自动实现接口方法
     }
 }
